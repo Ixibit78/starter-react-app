ea2b0db541233b43f818a42d726d7196d6612817
Ixibit78 <157749012+Ixibit78@users.noreply.github.com>
2024-01-25 22:28:31 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and sosi pidor ebaniy cyclic to reload.
+          sosi pidor ebaniy cyclic
         </p>
         <a
           className="App-link"
